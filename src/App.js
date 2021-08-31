16f1f3ef42b837144baefff683c4a205e3b8cf57
PhillipJeffries <ggbox@mail.ru>
2021-08-31 12:55:39 +0400

    fonts

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>hallo</h1>
+      <h1 className="main-title">{"PhotoAlbum".toUpperCase() }</h1>
+      <span className="subtitle">by Dmitry Kornienko</span>
     </div>
   );
 }
